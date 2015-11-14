1058ddfbedbed0ef83224aa259e961b98b690f5e
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2015-11-14 13:44:13 +0100

    Cleaned up code and translated comments to english.

--- videoObjectTracker.py
+++ videoObjectTracker.py
@@ -1,54 +1,58 @@
 # -*- coding: utf-8 -*-
 """
-Python program til at finde røde cirkler i film.
+Python program for tracking certain markers in videos.
 
-Skrevet af: Henrik Skov Midtiby og Mads Dyrmann
-Dato: 2014-10-06
-
-Versionshistorik:
-2015-10-19: Understøttelse for OpenCV3.0
-            Tilføjet tjek for om video er færdiglæst
-            Ændret threshold værdi til også at virke på græs
+Written by: Henrik Skov Midtiby
+Date: 2015-11-14
 """
 
-import time
 import cv2
 import MarkerTracker
 
-def main():
-    cap = cv2.VideoCapture('input/2015-11-12 09.06.21.mp4')
-            
-    outputfile = open('output/positions.txt', 'w')
-    tracker = MarkerTracker.MarkerTracker(4,  51,  1.0)
-    counter = 0
+def main(videoFileToAnalyze, outputFile, orderOfMarker, sizeOfKernel):
+    # Open video file for reading and output file for writing.
+    cap = cv2.VideoCapture(videoFileToAnalyze)
+    outputfile = open(outputFile, 'w')
+    
+    # Initialize the marker tracker.
+    tracker = MarkerTracker.MarkerTracker(orderOfMarker,  sizeOfKernel,  1.0)
 
+    # Main loop
+    counter = 0
     while(cap.isOpened()):
         counter += 1
-        # Læs et nyt billede fra filmen.
+        # Read a new image from the file.
         ret, frame = cap.read()
         
-        #time.sleep(0.5)
+        # Halt if reading failed.
         if not ret:
             break
         
-        #frame = cv2.resize(frame, dsize = None, fx = 0.5, fy = 0.5)
+        # Cnovert image to grayscale.
         grayScaleImage = (cv2.split(frame)[0] + cv2.split(frame)[1] + cv2.split(frame)[2]) / 3.
 
+        # Locate marker in image.
         (xm,  ym)  = tracker.locateMarker(grayScaleImage)
-        print("(x, y): %3d %3d" % (xm, ym))
+
+        # Write determined marker position to file.
         outputfile.write("%3d\t%3d\t%3d\n" % (counter, xm, ym))
 
-#        cv2.line(frame, point1, point2, (255, 0, 0), 4)
+        # Mark the center of the marker and show the annotated image.
         cv2.circle(frame, (xm,  ym), 20, (255, 0, 255), -1)
-
         cv2.imshow('frame', frame)
-
+        
+        # Break the look if the key 'q' was pressed. 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
         
     outputfile.close()
     return    
 
-
-main()
+# Launch the program.
+videoFileToAnalyze = 'input/2015-11-12 09.06.21.mp4'
+outputFile = 'output/positions.txt'
+orderOfMarker = 4
+sizeOfKernel = 51
+main(videoFileToAnalyze, outputFile, orderOfMarker, sizeOfKernel)
+    
 
