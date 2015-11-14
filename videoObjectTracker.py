2ab2c7283fadd71997d828dcf268a695b62ec00b
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2015-11-14 13:34:18 +0100

    Ported markertracker to cv2 and shrunk it to the bare minimals. Also tidied up the video analyzing script.

--- videoObjectTracker.py
+++ videoObjectTracker.py
@@ -11,41 +11,44 @@ Versionshistorik:
             Ændret threshold værdi til også at virke på græs
 """
 
-import cv2
-import numpy as np
 import time
+import cv2
+import MarkerTracker
 
 def main():
-    cap = cv2.VideoCapture('input/2015-11-11 12.50.19.mp4')
-    
-    if cv2.__version__[0]=='3': #if opencv 3.0
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    else: #if opencv<30.
-        fourcc = cv2.cv.CV_FOURCC(*'XVID')
-        
-    VidOut = cv2.VideoWriter('output/output.avi',fourcc,20,(720,480))    
-    
+    cap = cv2.VideoCapture('input/2015-11-12 09.06.21.mp4')
+            
+    outputfile = open('output/positions.txt', 'w')
+    tracker = MarkerTracker.MarkerTracker(4,  51,  1.0)
+    counter = 0
+
     while(cap.isOpened()):
+        counter += 1
         # Læs et nyt billede fra filmen.
         ret, frame = cap.read()
+        
         #time.sleep(0.5)
         if not ret:
-            break        
+            break
         
-        # Vis billedet på skærmen.
+        #frame = cv2.resize(frame, dsize = None, fx = 0.5, fy = 0.5)
+        grayScaleImage = (cv2.split(frame)[0] + cv2.split(frame)[1] + cv2.split(frame)[2]) / 3.
+
+        (xm,  ym)  = tracker.locateMarker(grayScaleImage)
+        print("(x, y): %3d %3d" % (xm, ym))
+        outputfile.write("%3d\t%3d\t%3d\n" % (counter, xm, ym))
+
+#        cv2.line(frame, point1, point2, (255, 0, 0), 4)
+        cv2.circle(frame, (xm,  ym), 20, (255, 0, 255), -1)
+
         cv2.imshow('frame', frame)
-
-        
-#                    point4 = (frame.shape[1], cy) 
-#                    cv2.line(frame, point1, point2, (255, 0, 0), 4)
-#                   cv2.circle(frame, (cx, cy), 20, (255, 0, 255), -1)
-
-        VidOut.write(frame)
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
-    VidOut.release()    
+        
+    outputfile.close()
     return    
 
 
 main()
+
